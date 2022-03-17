440f5a3a505face0f7e99ebc6f6c29f19c9e144c
xuantien8a1@gmail.com <xuantien8a1@gmail.com>
2022-03-17 21:15:49 +0700

    Hoàn thành buổi 3 đến trang 37

--- Lab3_NguyenXuanTien_1911066400/Controllers/CoursesController.cs
+++ Lab3_NguyenXuanTien_1911066400/Controllers/CoursesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Lab3_NguyenXuanTien_1911066400.Models;
+using Lab3_NguyenXuanTien_1911066400.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,35 @@ namespace Lab3_NguyenXuanTien_1911066400.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
+        [Authorize]
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place =viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
