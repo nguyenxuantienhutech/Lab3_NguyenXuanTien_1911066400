168ff6225fb9df9926a2a2606264733d845b2311
xuantien8a1@gmail.com <xuantien8a1@gmail.com>
2022-03-21 11:11:43 +0700

    Buoi 4 hoan thanh chuc nang Log Off

--- Lab3_NguyenXuanTien_1911066400/ViewModels/CourseViewModel.cs
+++ Lab3_NguyenXuanTien_1911066400/ViewModels/CourseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Lab3_NguyenXuanTien_1911066400.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,15 @@ namespace Lab3_NguyenXuanTien_1911066400.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureData]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
