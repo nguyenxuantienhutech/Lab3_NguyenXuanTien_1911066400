221c57e7c48973e620225a3701c9d3efebf97319
xuantien8a1@gmail.com <xuantien8a1@gmail.com>
2022-03-28 11:43:12 +0700

    Buoi 5 postman

---
+++ Lab3_NguyenXuanTien_1911066400/Controllers/AttendancesController.cs
@@ -0,0 +1,36 @@
+﻿using Lab3_NguyenXuanTien_1911066400.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab3_NguyenXuanTien_1911066400.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend ([FromBody] int courseID)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == courseID))
+                return BadRequest("The Attendace already exists !");
+            var attendance = new Attendance
+            {
+                CourseId = courseID,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
